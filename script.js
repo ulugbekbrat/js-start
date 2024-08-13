cf2261ff31a3eef26ed5dc60c25b62a1d9518edb
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-08-13 10:24:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var a = prompt ("Ismingizni kiriting")
+
+console.log('Ism:',a)
+
+var a = prompt ("Familiyangizni kiriting")
+
+console.log('Familiya:',a)
+
+var a = prompt ("Yoshingizni kiriting")
+
+console.log('Yosh:',a)
+
+var a = prompt ("Yoqtirgan faningizni kiriting")
+
+console.log('Yoqtirgan fan:',a)
+
+var a = prompt ("Yashaydigan tumaningizni kiriting")
+
+console.log('Yashash manzili:',a)
+
+let b = alert ('malumotlaringizni kiritkaninguz uchun rahmat')
